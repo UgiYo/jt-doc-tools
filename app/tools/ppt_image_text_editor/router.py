161c0c0c4f2febf18adef0e6362f0dfef6a609a0
Ugi Yo <dog310764@hotmail.com>
2026-09-14 13:32:28 +0800

    Serialize OCR and PPTX conversion workload

--- app/tools/ppt_image_text_editor/router.py
+++ app/tools/ppt_image_text_editor/router.py
@@ -11,7 +11,7 @@ from app.tools.editable_slides.pptx_io import export_pptx
 from .image_edit import available_fonts,edit_text,image_format_for_path,to_png
 from .pptx_core import list_slide_images,read_media,replace_media
 router=APIRouter();_ID_RE=re.compile(r"^[a-f0-9]{32}$")
-_OCR_LIMIT=asyncio.Semaphore(1);_CONVERT_LIMIT=asyncio.Semaphore(1);_jobs={};_convert_jobs={};_tasks=set()
+_OCR_LIMIT=asyncio.Semaphore(1);_CONVERT_LIMIT=_OCR_LIMIT;_jobs={};_convert_jobs={};_tasks=set()
 def _job_view(uid):
  job=_jobs.get(uid)
  if not job:return None
